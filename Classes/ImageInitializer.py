3068cac96cabdb27121adf5307ebdcaa219c97f7
Quinn Pulley <quinnpulley@gmail.com>
2024-10-31 15:20:55 -0500

    image initializer stuff i think

---
+++ Classes/ImageInitializer.py
@@ -0,0 +1,15 @@
+import Image
+import Color
+
+class ImageInitializer:
+    """Class to initialize an Image"""
+    
+    def __init__(self) -> None:
+        pass
+    
+    def import_image(self, path: str) -> Image:
+        pass
+    
+    def create_blank_canvas(self, width: int, height: int, color: Color) -> Image:
+        # this needs to create a numpy array with specified size (width, height) and backgroud color (color) but idk how to do that
+        pass
---
+++ Classes/JpegImporter.py
@@ -0,0 +1,14 @@
+import ImageInitializer
+import Image
+from PIL import Image as PilImage
+import numpy as np
+
+class JpegImporter(ImageInitializer):
+    """Class to import an Image of type .jpeg"""
+    
+    def __init__(self) -> None:
+        super.__init__()
+    
+    def import_image(self, path: str) -> Image:
+        image = PilImage.open(path).convert('RGB')
+        return Image(np.array(image))
