ee74d58ae2ce136b025882bbc7ec134545a16292
GameDJ <9010034+GameDJ@users.noreply.github.com>
2024-10-31 14:52:29 -0500

    populated Arguments, and added type annotations to Image

--- Classes/Image.py
+++ Classes/Image.py
@@ -1,6 +1,8 @@
+from numpy import ndarray
+
 class Image:
-    def __init__(self, img_array):
+    def __init__(self, img_array: ndarray) -> None:
         self.img_array = img_array
         
-    def get_img_array(self):
+    def get_img_array(self) -> ndarray:
         return self.img_array
