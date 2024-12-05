5b354f2cee8fe50165d8996ad82a10f139014fe7
GameDJ <9010034+GameDJ@users.noreply.github.com>
2024-12-05 14:14:57 -0600

    edit superclass returns image

--- Classes/edit/Edit.py
+++ Classes/edit/Edit.py
@@ -1,8 +1,8 @@
 #This is a superclass that defines the behavior of all of its subclasses
 
 from Classes.info.Arguments import Arguments
+from Classes.image.Image import Image
 
 class Edit():
-  
-  def edit(args: Arguments):
+  def edit(self, args: Arguments) -> Image:
     pass
