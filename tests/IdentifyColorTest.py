dfebbeae06c92e4ce42c3474df42b9ad4a0c3db5
acasner1 <acasner1@icloud.com>
2024-12-07 15:24:25 -0600

    fixing identify color

--- tests/IdentifyColorTest.py
+++ tests/IdentifyColorTest.py
@@ -24,7 +24,7 @@ class TestIdentifyColorFromImage(unittest.TestCase):
         dummy_image = Image(dummy_image_array)
         self.handler.hist.add_record(dummy_image, "Dummy image for testing")
         color = self.handler.get_color_at_pixel(0,0)
-        self.assertEqual(color.all(), (0,0,255),)
+        self.assertListEqual(color, (0,0,255))
     
     def test_edge_pixel(self):
         self.handler = RequestHandler()
@@ -40,8 +40,9 @@ class TestIdentifyColorFromImage(unittest.TestCase):
         
         dummy_image = Image(dummy_image_array)
         self.handler.hist.add_record(dummy_image, "Dummy image for testing")
-        color = self.handler.get_color_at_pixel(1,2)
-        self.assertEqual(color, (255,0,255))
+        color = self.handler.get_color_at_pixel(2,1)
+        print(color)
+        self.assertTupleEqual(color, (255,0,255))
 
     def test_invalid_pixel_out_of_bounds(self):
         self.handler = RequestHandler()
@@ -83,18 +84,6 @@ class TestIdentifyColorFromImage(unittest.TestCase):
 
     def test_no_active_image(self):
         self.handler = RequestHandler()
-        dummy_image_array = np.array([
-            [[0, 0, 255], [0, 255, 0], [255,0,0]],
-            [[255, 128, 0], [255, 255, 0], [255, 0, 255]],
-            [[255, 255, 255], [128, 128, 128], [0, 0, 0]]
-            ])
-
-            #[blue, green, red]
-            #[orange, yellow, purple]
-            #[white, gray, black]
-        
-        dummy_image = Image(dummy_image_array)
-        self.handler.hist.add_record(dummy_image, "Dummy image for testing")
         self.handler.hist = None
         with self.assertRaises(AttributeError):
             self.handler.get_color_at_pixel(0,0)
