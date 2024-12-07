45958354a5ee71b053d9b5f9d3dd3227c43357fa
WillPlaetse <will.verplaetse@gmail.com>
2024-12-06 20:49:19 -0600

    more testing changes

--- tests/ImportImageTesting.py
+++ tests/ImportImageTesting.py
@@ -2,6 +2,26 @@ import unittest
 import numpy as np
 
 from Image import Image
+from ImageInitializer import ImageInitializer
+from PngImporter import PngImporter
+from JpegImporter import JpegImporter
+
 
 class ImportImageTesting(unittest.TestCase):
-    
+    def testing_png_import(self):
+        importer = JpegImporter()
+        image = importer.import_image('./nyx.png')
+        self.assertIsNotNone(image)
+
+    def testing_jpeg_import(self):
+        importer = PngImporter()
+        image = importer.import_image('./nyx_cyndaquil.jpg')
+        self.assertIsNotNone(image)
+
+    def testing_
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
