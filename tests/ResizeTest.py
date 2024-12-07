5703f4cc1ec13002925fc650c855930d1052e740
WillPlaetse <will.verplaetse@gmail.com>
2024-12-07 14:59:23 -0600

    MORE TESTING 3

--- tests/ResizeTest.py
+++ tests/ResizeTest.py
@@ -6,43 +6,27 @@ import unittest
 import cv2
 import sys
 sys.path.append("../image-editor")
-from Classes.image.Image import Image
-from Classes.info.Arguments import Arguments
-from Classes.edit.SizeEditor import SizeEditor
+from Classes.RequestHandler import RequestHandler
 
 class ResizeTesting(unittest.TestCase):
   def testing_negative_size(self):
-    orig = np.full( (10,10,3), (80, 128, 40), dtype=np.uint8)
-
-    image = Image(orig)
-    the_args = Arguments(image)
-    the_args.add_dimensions( (-100, -100))
-    editor = SizeEditor()
-    edited = editor.edit(the_args)
-    resized = cv2.resize(orig, (100, 100))
-    correct_im = Image(resized)
-    self.assertEqual(edited, correct_im)
+    
+    handler = RequestHandler()
+    handler.create_canvas(10, 10, (78, 62, 128))
+    self.assertEqual( handler.resize((100,100)), True)
+    
 
 
   def testing_positive_size(self):
-    orig = np.full( (10,10,3), (80, 128, 40), dtype=np.uint8)                  
-    image = Image(orig)
-    the_args = Arguments(image)
-    the_args.add_new_size(50, 50)
-    editor = SizeEditor()
-    edited = editor.edit(the_args)
+    handler = RequestHandler()
+    handler.create_canvas(10, 10, (78, 62, 128))
+    self.assertEqual( handler.resize((100,100)), True)
 
-    resized = cv2.resize(orig, (50, 50))
-    correct_im = Image(resized)
-    self.assertEqual(edited, correct_im)
 
   def testing_zero_size(self):
-    orig = np.full( (10,10,3), (80, 128, 40), dtype=np.uint8)
-    image = Image(orig)
-    the_args = Arguments(image)
-    the_args.add_dimensions((0,0))
-    editor = SizeEditor()
-    self.assertRaises(ValueError, editor.edit(the_args))
+    handler = RequestHandler()
+    handler.create_canvas(10, 10, (78, 62, 128))
+    self.assertRaises(ValueError, RequestHandler.resize, 0, 0)
     
 
 if __name__ == '__main__':
