b2b7ca9d8957bd5a78e7c43ab0978e174b5e91ec
Quinn Pulley <quinnpulley@gmail.com>
2024-12-07 14:53:52 -0600

    return types!!!!!!

--- Classes/info/Arguments.py
+++ Classes/info/Arguments.py
@@ -16,31 +16,40 @@ class Arguments:
         self.args[AT.IMAGE] = image
         return not key_already_existed
             
-    def add_selection(self, selection: Selection) -> None:
+    def add_selection(self, selection: Selection) -> bool:
         """Coordinates of actual selection"""
         self.args[AT.SELECTION] = selection
+        return True
         
-    def add_selection2(self, selection: Selection) -> None:
+    def add_selection2(self, selection: Selection) -> bool:
         """Coordinates of selection2; e.g. shape draw area"""
         self.args[AT.SELECTION2] = selection
+        return True
     
     # eventually these will prob be enums and stuff
-    def add_filter(self, filter_type: AT) -> None:
+    def add_filter(self, filter_type: AT) -> bool:
         """Name of filter to apply"""
         self.args[AT.FILTER] = filter_type
-    def add_amount(self, amount: float) -> None:
+        return True
+    
+    def add_amount(self, amount: float) -> bool:
         """An amount, usually to inform a filter strength"""
         self.args[AT.AMOUNT] = amount
+        return True
             
-    def add_shape(self, shape_type: ShapeType) -> None:
+    def add_shape(self, shape_type: ShapeType) -> bool:
         """Name of the chosen shape"""
         self.args[AT.SHAPE] = shape_type
-    def add_color(self, color: tuple[int, int, int]) -> None:
+        return True
+    
+    def add_color(self, color: tuple[int, int, int]) -> bool:
         """An RGB color tuple, usually to inform the drawing color"""
         self.args[AT.COLOR] = color
+        return True
 
-    def add_dimensions(self, dimensions: tuple[int, int]) -> None:
+    def add_dimensions(self, dimensions: tuple[int, int]) -> bool:
         self.args[AT.DIMENSIONS] = dimensions
+        return True
 
     def get_args(self) -> dict:
         return self.args
