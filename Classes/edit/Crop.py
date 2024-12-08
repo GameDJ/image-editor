abd6bb93eac95df5a73c98dfbe865a3a952f6c16
Quinn Pulley <quinnpulley@gmail.com>
2024-12-08 11:44:09 -0600

    fixing crop

--- Classes/edit/Crop.py
+++ Classes/edit/Crop.py
@@ -18,7 +18,7 @@ class Crop(Edit):
 
     #Bounds checking
     if not (bbox[0] < 0 or bbox[0] > len(image_array[0]) or bbox[1] < 0 or bbox[1] > len(image_array) or bbox[2] < 0 or bbox[2] > len(image_array[0]) or bbox[3] < 0 or bbox[3] > len(image_array) ):
-      image_array = image_array[bbox[0]:bbox[2], bbox[1]:bbox[3]]
+      image_array = image_array[bbox[1]:bbox[3], bbox[0]:bbox[2]]
       image.set_img_array(image_array)
       
     return image
